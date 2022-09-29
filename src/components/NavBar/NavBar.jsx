912aed4b62b00847d8ad58737cf7552545e6f8dd
elijiah silvereagle <elijiahsilvereagle@gmail.com>
2022-09-29 11:03:01 -0700

    starting deep api

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -32,10 +32,14 @@ export default function NavBar({ user, setUser, handleLogOut, handleColor }) {
         <Link to="/Spells" className="text-info btn btn-lg btn-outline-black">Spells</Link>
         &nbsp;&nbsp;
         <NavDropdown title="Background" className="text-info btn btn-lg btn-outline-black" id="basic-nav-dropdown">
-            <NavDropdown.Item type="submit" onClick={handleColor} name="B13" value="B13" className="text-primary btn btn-lg">Blue</NavDropdown.Item>
-            <NavDropdown.Item type="submit" onClick={handleColor} name="G13" value="G13" className="text-success btn btn-lg">Green</NavDropdown.Item>
-            <NavDropdown.Item type="submit" onClick={handleColor} name="R13" value="R13" className="text-danger btn btn-lg">Red</NavDropdown.Item>
-            <NavDropdown.Item type="submit" onClick={handleColor} name="Y13" value="Y13" className="text-warning btn btn-lg">Yellow</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="B4" value="B4" className="text-primary btn btn-lg">Blue - Smooth</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="B13" value="B13" className="text-primary btn btn-lg">Blue - Stressed</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="G4" value="G4" className="text-success btn btn-lg">Green - Smooth</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="G13" value="G13" className="text-success btn btn-lg">Green - Stressed</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="R4" value="R4" className="text-danger btn btn-lg">Red - Smooth</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="R13" value="R13" className="text-danger btn btn-lg">Red - Stressed</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="Y4" value="Y4" className="text-warning btn btn-lg">Yellow - Smooth</NavDropdown.Item>
+            <NavDropdown.Item type="submit" onClick={handleColor} name="Y13" value="Y13" className="text-warning btn btn-lg">Yellow - Stressed</NavDropdown.Item>
         </NavDropdown>
         {   user 
         ? <Link to="" onClick={handleLogOut} className="text-warning btn btn-lg btn-outline-black">Log Out</Link>
