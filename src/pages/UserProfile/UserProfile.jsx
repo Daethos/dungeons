a231a686a016afeac439379883dde633c8a8ab32
elijiah silvereagle <elijiahsilvereagle@gmail.com>
2022-10-02 19:17:01 -0700

    tighter flow and ux

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Carousel from 'react-bootstrap/Carousel';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import * as monstersAPI from '../../utils/monsterApi';
@@ -23,6 +24,11 @@ export default function UserProfile({ loggedUser, handleLogout, setUser }) {
     const [isSaved, setIsSaved] = useState(true)
     const [onProfile, setOnProfile] = useState(true)
     const BUCKET_START = 'https://collectionbucketman.s3.amazonaws.com/dungeons/';
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
 
     async function filterMonsters(results) {
         console.log(results, '<- Results in filterMonsters')
@@ -62,6 +68,7 @@ export default function UserProfile({ loggedUser, handleLogout, setUser }) {
                         key={allMonsters[i]._id}
                         deleteMonster={deleteMonster}
                         isSaved={true}
+                        onProfile={onProfile}
                     />
                 </Col>
             )
@@ -154,7 +161,119 @@ export default function UserProfile({ loggedUser, handleLogout, setUser }) {
                 </>
                 : ''   
             }
-            {monsters.map((monster) => {
+            {
+                monsters.length > 0
+                ? monsters.map((monster) => {
+                    return (
+                        <SolaMonstra
+                            monster={monster}
+                            key={monster._id}
+                            deleteMonster={deleteMonster}
+                            editMonster={editMonster}
+                            isSaved={isSaved}
+                            onProfile={onProfile}
+                        />
+                    )
+                })
+                :
+                <Row>
+                <Col md={{span: 8, offset: 2}}>
+            <Carousel activeIndex={index} id="carousel" onSelect={handleSelect} fade>
+                <Carousel.Item>
+                <img 
+                    src={BUCKET_START + 'no-monsters.png'} 
+                    alt="To Be Determined!" 
+                    
+                    id="carousel-one"
+                    className="d-block w-100"
+                />
+                    <Carousel.Caption>
+                    <h3 className="text-black">So You Have No Monsters?</h3>
+                    <p id="car-cap" border="warning" className="text-warning bg-black p-3">Never fear! Simply reach above and click on 
+                    Monsters to start your journey.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img 
+                    src={BUCKET_START + 'monster-tab.png'} 
+                    alt="To Be Determined!" 
+                    
+                    id="loading" 
+                    className="d-block w-100"
+                />
+                    <Carousel.Caption>
+                    <h3 className="text-black">Monster Retrieval</h3>
+                    <p id="car-cap" className="text-warning bg-black p-3">And voila! Hot, delicious monsters served right to you!
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img 
+                    src={BUCKET_START + 'monster-scroll.png'} 
+                    alt="To Be Determined!" 
+                    
+                    id="loading"
+                    className="d-block w-100" 
+                />
+                    <Carousel.Caption>
+                    <h3 className="text-black">Monster Scroll</h3>
+                    <p id="car-cap" className="text-warning bg-black p-3">
+                        Scrolling is the default option at hand, granting every monster available with effort, but...
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img 
+                    src={BUCKET_START + 'monster-search.png'} 
+                    alt="To Be Determined!" 
+                    
+                    id="loading"
+                    className="d-block w-100" 
+                />
+                    <Carousel.Caption>
+                    <h3 className="text-black">Monster Search</h3>
+                    <p id="car-cap" className="text-warning bg-black p-3">
+                        Simply type any part of the name in question and it ought to bring you to your desired result.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img 
+                    src={BUCKET_START + 'monster-detail.png'} 
+                    alt="To Be Determined!" 
+                    
+                    id="loading"
+                    className="d-block w-100" 
+                />
+                    <Carousel.Caption>
+                    <h3 className="text-black">Monster Detail</h3>
+                    <p id="car-cap" className="text-warning bg-black p-3">
+                        And here grants a significantly better view on the Monster or otherwise has to offer.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                <img 
+                    src={BUCKET_START + 'monster-saved.png'} 
+                    alt="To Be Determined!" 
+                    
+                    id="loading"
+                    className="d-block w-100" 
+                />
+                    <Carousel.Caption>
+                    <h3 className="text-black p-2">Monster Saved</h3>
+                    <p id="car-cap" className="text-warning bg-black p-3">
+                        With the click of a button it's that easy. Clicking on your Photo Icon above will
+                        bring you back to your home page and allow you to update and customize your monster of choice.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            </Col>
+            </Row>
+            }
+
+            {/* {monsters.map((monster) => {
                 return (
                     <SolaMonstra
                         monster={monster}
@@ -165,7 +284,7 @@ export default function UserProfile({ loggedUser, handleLogout, setUser }) {
                         onProfile={onProfile}
                     />
                 )
-            })}
+            })} */}
             {/* <MonsterCard monsters={monsters} deleteMonster={deleteMonster} /> */}
         </div>
     );
